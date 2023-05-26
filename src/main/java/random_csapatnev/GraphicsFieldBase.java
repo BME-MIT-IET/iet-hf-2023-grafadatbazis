39ddab2485080418beafd2226aaab268dec76aeb
Mysteryusy <valentinjozsef0@gmail.com>
2023-05-26 18:22:11 +0200

    GraphicsFieldBase.java adattagjainak és függvényeinek átnevezése

--- src/main/java/random_csapatnev/GraphicsFieldBase.java
+++ src/main/java/random_csapatnev/GraphicsFieldBase.java
@@ -8,13 +8,13 @@ public class GraphicsFieldBase extends JPanel
 {
 	int graphicsx;
 	int graphicsy;
-	public GraphicsFieldBase(int _x, int _y)
+	public GraphicsFieldBase(int inputX, int inputY)
 	{
 		super();
 		this.setBorder(new MatteBorder(1,1,1,1, Color.BLACK));
 		this.setLayout(null);
 		
-		graphicsx = _x;
-		graphicsy = _y;
+		graphicsx = inputX;
+		graphicsy = inputY;
 	}
 }
