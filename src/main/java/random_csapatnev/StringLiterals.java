c55074fa08b6e88440bc59f9dfaa212dad491200
mate-bak <bak.mate98@protonmail.com>
2023-05-27 14:58:31 +0200

    Javítások StringLiterals fájlban

--- src/main/java/random_csapatnev/StringLiterals.java
+++ src/main/java/random_csapatnev/StringLiterals.java
@@ -1,6 +1,6 @@
 package random_csapatnev;
 
-public class StringLiterals {
+public final class StringLiterals {
     public static final String AMN_VACCINE = "AmnesiaVaccine";
     public static final String PAR_VACCINE = "ParalyzingVaccine";
     public static final String PROT_VACCINE = "ProtectiveVaccine";
@@ -12,4 +12,6 @@ public class StringLiterals {
     public static final String CLOAK_PATH = ".\\src\\main\\java\\images\\cloak.png";
     public static final String GLOVES_PATH = ".\\src\\main\\java\\images\\gloves2.png";
     public static final String SACK_PATH = ".\\src\\main\\java\\images\\sack.png";
+
+    private StringLiterals(){}
 }
