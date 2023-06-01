ca2cc21dd9a30da4f2b826ea6901400da4ea25d7
davidok0 <doncidavid@gmail.com>
2023-06-01 20:11:43 +0200

    FieldTest
    "bearInteract" metódus unit tesztek

--- src/test/java/random_csapatnev/FieldTest.java
+++ src/test/java/random_csapatnev/FieldTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
@@ -33,7 +34,12 @@ public class FieldTest {
     public void setup() {
         field = new Field(0, 0);
         bearMock = mock(Bear.class);
+        character1Mock = mock(Character.class);
+        character2Mock = mock(Character.class);
+
         bearMock.name = "bear";
+        character1Mock.name = "character";
+        character2Mock.name = "virologist";
     }
 
 	@Test
@@ -83,8 +89,34 @@ public class FieldTest {
         assertFalse(isNeighbour);
     }
 	
+    @Test
+    public void bearInteract_CallsCharacterInteract_WhenCharacterNameDoesNotStartWithB() {
+    	//Arrange
+    	field.characters.add(character1Mock);
+        
+        //Act
+        field.bearInteract(bearMock);
+        
+        //Assert
+        verify(bearMock, times(1)).characterInteract(character1Mock);
+    }
+    
+    @Test
+    public void bearInteract_CallsCharacterInteractOnMultipleCharacters_WhenCharactersNameDoesNotStartWithB() {
+    	//Arrange
+    	field.characters.add(character1Mock);
+        field.characters.add(character2Mock);
+        
+        //Act
+        field.bearInteract(bearMock);
+        
+        //Assert
+        verify(bearMock, times(1)).characterInteract(character1Mock);
+        verify(bearMock, times(1)).characterInteract(character2Mock);
+    }
+	
 	@Test
-	public void bearInteract_NoCharacters_NoInteraction() {
+	public void bearInteract__DoesNotCallCharacterInteract_WhenCharactersListIsEmpty() {
 		// Act
 		field.bearInteract(bearMock);	// Nincs karakter a mezőn
 
